2c4d7081454dc01ff03d408e3245c303394023c9
Rushikesh <rvp6123@gmail.com>
2026-05-06 08:49:23 +0530

    feat: add Google sign-in functionality to SignUp component

--- react-firebase/src/pages/SignUp.jsx
+++ react-firebase/src/pages/SignUp.jsx
@@ -1,9 +1,9 @@
 import React, { use } from 'react'
 import { useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from "../firebase";
 const auth = getAuth(app);
-
+const googleProvider = new GoogleAuthProvider();
 
 const SignUp = () => {
 
@@ -13,6 +13,10 @@ const SignUp = () => {
     const createUser = () =>{
         createUserWithEmailAndPassword(auth, email, password).then(value=>alert("success"))
     }
+
+    const signInWithGoogle = () =>{
+        signInWithPopup(auth, googleProvider)
+    }
   return (
     <div className="signup-page">
         <h1>Sign Up Page</h1>
@@ -21,6 +25,8 @@ const SignUp = () => {
         <br /><br />
         <label>Password</label>
         <input type="password" onChange={e=> setPassword(e.target.value)} value={password} required placeholder="Enter Your Password" />
+        <br /><br />
+        <button onClick={signInWithGoogle}>Sign-in-with-google</button>
         <br /><br />
         <button onClick={createUser}>SignUp</button>
     </div>
